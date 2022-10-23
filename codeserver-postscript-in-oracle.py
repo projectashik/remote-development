545855c9efa11c31a6173d8863837368573fdf37
Ashik Chapagain <47524279+projectashik@users.noreply.github.com>
2022-10-23 23:23:56 +0545

    Update codeserver-postscript-in-oracle.py

--- codeserver-postscript-in-oracle.py
+++ codeserver-postscript-in-oracle.py
@@ -4,6 +4,7 @@ import os
 commands = [
     "sudo ufw enable",
     "sudo ufw allow \"Nginx Full\"",
+    "sudo ufw reload",
     "sudo iptables -I INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT",
     "sudo iptables -I OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT"
 ]
